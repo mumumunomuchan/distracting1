686fb6ef363f0387ef5903c039443a3ea4fb6200
Misaki <misaki@Misakis-MacBook-Air.local>
2023-05-31 13:59:13 -0700

    only destructing

---
+++ destructing.js
@@ -0,0 +1,58 @@
+console.log(numPlanets);//8
+console.log(yearNeptuneDiscoverd); //1846
+
+console.log(discoveryYears); 
+let var1={yearNeptuneDiscovered:1846, yearMarsDiscovered: 1659};
+
+/* Your name is Alejandro, favotite color is purple
+
+Your name is Melissa, favorite color is green
+
+Your name is undefined and you like green */
+
+console.log(first);//Maya
+console.log(second);//arisa
+console.log(third); //Chi
+
+console.log(raindrops)//;Raindrops on roses"
+console.log(whiskers)//;"whiskers on kittens"
+console.log(aFewOfMyFavoriteThings);////"Bright copper kettles",
+//"warm woolen mittens",
+//"Brown paper packages tied up with strings"
+
+console.log(numbers);10,30,20
+
+var obj = {
+       numbers: {
+         a: 1,
+         b: 2
+       }
+     };
+     
+     var a = obj.numbers.a;
+     var b = obj.numbers.b;
+
+/* Write an ES2015 Version */
+
+let obj = {
+      numbers:{
+       a:1,
+       b:2
+      }
+};
+let a = obj.numbers.a;
+let b = obj.numbers.b;
+
+
+var arr = [1, 2];
+var temp = arr[0];
+arr[0] = arr[1];
+arr[1] = temp;
+
+/* Write an ES2015 Version */
+
+let arr = [1,2];
+[arr[0],arr[1]] = [arr[1], arr[0]];
+
+const raceResults = ([fitst, second, third, ...rest]) => ['Tom', 'Margaret', 'Allison', 'David', 'Pierre'];
+
